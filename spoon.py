7a90be1da249ea7581f052565b906370ee85ada0
GaryCheng <d9369604@gmail.com>
2018-04-27 11:04:27 +0800

    Add some new mechanism
    1. Add 'size' parameter
    2. Remove fail screenshot after test finished

--- spoon.py
+++ spoon.py
@@ -5,9 +5,10 @@ from shutil import rmtree
 
 home_path = os.path.expanduser("~")
 apk_output_folder = home_path + "/KKBOX/kkbox_android/KKBOX/build/outputs/apk"
-spoon_folder = home_path + "/Desktop/important/spoon"
+spoon_folder = home_path + "/Desktop/important/spoonHelper"
 output_folder = spoon_folder + "/output"
 open_report_cmd = "open " + output_folder + "/result1/result.html"
+deviceID = "emulator-5554"
 
 
 def execute_test(parameters=None):
@@ -23,6 +24,8 @@ def execute_test(parameters=None):
     generate_report()
 
     open_report(parameter_dict)
+
+    os.system("adb -s {} shell rm -rf /mnt/sdcard/app_spoon-screenshots/*".format(deviceID))
 
 
 def open_report(parameter):
@@ -63,7 +66,7 @@ def get_parameter(parameters):
     :param parameters: Arguments in list type
     :return: Dictionary have arguments
     """
-    parameter_dict = {'freq': 1, 'notify': False}
+    parameter_dict = {'freq': 1, 'notify': "true"}
 
     if parameters:
         for parameter in parameters:
@@ -89,17 +92,23 @@ def get_spoon_cmd(parameter_dict):
     :return: Command in str format
     """
     class_name_parameter = " --e class=com.kkbox.tests.e2e."
+    # class_name_parameter = " --e class=com.kkbox.tests.e2e.cucumber.feature."
+    # class_name_parameter = " --e package=com.kkbox.tests.e2e."
+    test_case_size = " --size "
     spoon_cmd = "java -jar {0}/spoon-runner-1.7.1-jar-with-dependencies.jar \
              --output {1}/result \
              --grant-all \
              --no-animations \
-             --size large \
              --apk {2}/KKBOX-play-debug.apk \
              --test-apk {2}/KKBOX-play-debug-androidTest.apk".format(spoon_folder, output_folder, apk_output_folder)
 
     if 'class' in parameter_dict:
         class_name_parameter += parameter_dict.get('class')
         spoon_cmd += class_name_parameter
+
+    if 'size' in parameter_dict:
+        test_case_size += parameter_dict.get('size')
+        spoon_cmd += test_case_size
 
     return spoon_cmd
 
